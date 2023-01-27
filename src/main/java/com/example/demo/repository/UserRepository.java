06982045999ec4665543b5a8dd86f7e1ac2b5e27
Michael Thomas <mike.ahpong@gmail.com>
2023-01-27 12:36:06 +0800

    updated user repository

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.User;
 
-public interface UserRepository extends JpaRepository<Todo, Long>{
+public interface UserRepository extends JpaRepository<User, Long>{
 
 }
