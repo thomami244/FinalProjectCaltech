9b681694418063c2b975a9c4524767f3560ee38c
Michael Thomas <mike.ahpong@gmail.com>
2023-02-03 11:35:24 +0800

    added ProductServiceImpl.java

---
+++ src/main/java/com/example/demo/services/ProductServiceImpl.java
@@ -0,0 +1,58 @@
+package com.example.demo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Product;
+
+import com.example.demo.model.Product;
+import com.example.demo.repository.ProductRepository;
+
+@Service(value = "productService")
+public class ProductServiceImpl implements ProductService {
+	
+
+	
+	private ProductRepository productRepository ;
+	
+	public ProductServiceImpl() {
+		super();
+	}
+	
+	
+	public ProductServiceImpl(ProductRepository productRepository) {
+		super();
+		System.out.println("spring ioc container invoked constructor to assemble 'productRepository' bean");
+		this.productRepository = productRepository;
+	}
+
+	@Autowired
+	public void setProductRepository(ProductRepository productRepository) {
+		System.out.println("spring ioc container invoked setter method to assemble 'productRepository' bean");
+		this.productRepository = productRepository;
+	}
+
+	@Override
+	public List<Product> findAll() {
+		return productRepository.findAll();
+	}
+
+	@Override
+	public Product findById(long theId) {
+		return productRepository.findById(theId).get();
+	}
+	
+	
+	@Override
+	public Product save(Product theProduct) {
+		return productRepository.save(theProduct);
+	}
+
+	@Override
+	public Product deleteById(long theId) {
+		 productRepository.deleteById(theId);
+		 return null;
+	}
+
+
+}
